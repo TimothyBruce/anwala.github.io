93dc6cc3c28f3791c2e5f49ec4b982e0f5d2c869
Timothy Bruce <tim.bruce9@gmail.com>
2019-03-06 21:54:07 -0500

    Completed question one, but it is not verified to be totally done.

--- cs532-s19/assignments/A4/A4.py
+++ cs532-s19/assignments/A4/A4.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
-import math as m
 import statistics as s
+import matplotlib.pyplot as plt
+import pylab
+
 
 f = open('acnwala-friendscount.csv', 'r')                    # Get the links.
 
@@ -16,3 +18,33 @@ print(data)
 print("The mean number of friends is: " + str(s.mean(data)))
 print("The standard deviation of friends is: " + str(s.stdev(data)))
 print("The median of friends is: " + str(s.median(data)))
+
+data.append(len(data))
+data.sort()
+
+
+
+
+def histogramGenerator(input_data):  # Get the mementos and create a histogram.
+    y=[]
+    friendNames = []
+    for i in range(len(input_data)):
+        y.append(i)
+        if (input_data[i] == len(data)-1):
+            friendNames.append("A. Nwala")
+        else:
+            friendNames.append("f"+str(i))
+    hist = plt.figure(num=None, figsize=(16, 6))  # Create a Bar plot.
+                            #(BELOW) Create the bar chart.
+    barPlot = plt.bar(y, input_data, align='edge')
+
+    plt.xlabel('Friends')  #Label things.
+    plt.ylabel('Individual\'s number of friends. (LOG Scale)')
+    plt.yscale("log")
+    plt.title('Alexander Nwala\'s friends in 2014 vs. Each friend\'s number of firends.')
+    plt.xticks(y, friendNames, ha='right', rotation=45, fontsize=7)
+
+    hist.savefig("foo.pdf", bbox_inches='tight')  # Save the histogram as a vector PDF.
+    pylab.show()                                  #Display histogram in window for me.
+
+histogramGenerator(data)
