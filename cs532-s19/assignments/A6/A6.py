72a1a5b4a04b0f1659cdc039688a75a5e7c10bf8
Timothy Bruce <tim.bruce9@gmail.com>
2019-03-31 17:36:17 -0400

    Completed the coding portion of A6

--- cs532-s19/assignments/A6/A6.py
+++ cs532-s19/assignments/A6/A6.py
@@ -1,8 +1,86 @@
 #!/usr/bin/python
+import recommendations as r
+import os
+import multiprocessing as mp
+
+sub_id = 4                                              # The ID number of the user found to be similar to me.
+favorite_movie = "Blues Brothers, The (1980)"           # My favorite move from the data set.
+least_favorite_movie = "Citizen Kane (1941)"            # My least favorite movie from the data set.
+# I really don't like Citizen Kane
 
 
 def main():
-    print("Hello, World!")
+    prefs = r.loadMovieLens(os.getcwd() + "/ml-100k")   # Calculates the preferences using recommendations.py
+    movie_recommendations = r.getRecommendations(prefs, str(sub_id))  # Calculates all recommendations for sub user.
+
+    print("\nThe most recommended movies for " + get_user(sub_id) + " (which is the substitute) are:")
+    for i in range(5):                                  # This section prints out the recommendations for sub user.
+        u = movie_recommendations[i][1]
+        print(u)
+    print("\nThe least recommended movies for the substitute are: ")
+    for i in range(5):
+        u = movie_recommendations[len(movie_recommendations) - (i + 1)][1]
+        print(u)
+
+    similiar_item_data = r.calculateSimilarItems(prefs)  # Calculate all similar movie data.
+    print("\nFive similar movies to my favorite movie " + favorite_movie + " are:")
+    fav_mov_sim = similiar_item_data[favorite_movie][0:5]  # For real me, some recommended movies for my preferences.
+    for mov in fav_mov_sim:
+        print(mov[1])
+
+    print("\nFive similar movies to my least favorite movie " + least_favorite_movie + " are:")
+    lea_fav_mov_sim = similiar_item_data[least_favorite_movie][0:5]
+    for mov in lea_fav_mov_sim:
+        print(mov[1])
 
 
+def get_most_and_least_similiar_users():
+    """
+    Calculates users that are similar to the substitute user from problem one. Early on, I noticed that the program
+    took a while to execute, so I multi-threaded the sim_distance function to expedite execution.
+
+    :return:
+    Prints out data on the top five most similar and least similar users to the substitute user.
+    """
+    pool = mp.Pool(mp.cpu_count())
+    prefs = r.loadMovieLens(os.getcwd()+"/ml-100k")
+    similarity_data = [pool.apply(get_similarity_data, args=(prefs, other_id+1)) for other_id in range(943)]
+    pool.close()
+    similarity_data = sorted(similarity_data, key=lambda x: x[1])
+
+    print("The most similar users to " + get_user(sub_id) + " (which is the substitute) are:")
+    for i in range(5):
+        u = get_user(similarity_data[len(similarity_data)-(i+1)][0])
+        print(u)
+
+    print("\nThe least similar users are: ")
+
+    for i in range(5):
+        u = get_user(similarity_data[i][0]).split("\n")[0]
+        print(u)
+
+
+def get_similarity_data(prefs, other_id):   # This function is part of the multi-threading process for the above.
+    return [other_id, r.sim_distance(prefs, str(sub_id), str(other_id))]
+
+
+def get_user(user_id):
+    """
+    Returns the printed data about the user from u.user.
+
+    :param user_id:
+    The user ID from u.user as an integer.
+
+    :return:
+    The line containing the user's data from u.user.
+    """
+    f = open("ml-100k/u.user", 'r')
+    for line in f.readlines():
+        u = line.split("|")
+        if int(u[0]) == user_id:
+            return line.split("\n")[0]
+
+
+get_most_and_least_similiar_users()
+print("\n"*1)
 main()
--- cs532-s19/assignments/A6/A6Failed.py
+++ cs532-s19/assignments/A6/A6Failed.py
@@ -2,11 +2,13 @@
 
 """
 For Question One, some users that are closest in name and occupation to me.
-
-
 """
 
+
 class User:
+    """
+    This class holds user data for organized, easy access.
+    """
     user_id = None
     age = None
     gender = None
@@ -16,19 +18,27 @@ class User:
     inputString = ""
     reviews = []
 
-    def __init__(self, UID, Age, Gender, Occupation, Zip_Code, inputString):
-        self.user_id = int(UID)
-        self.age = int(Age)
-        self.gender = Gender
-        self.occupation = Occupation
-        self.zip_code = Zip_Code
-        self.inputString = inputString
+    def __init__(self, uid, age, gender, occupation, zip_code, input_string):
+        self.user_id = int(uid)
+        self.age = int(age)
+        self.gender = gender
+        self.occupation = occupation
+        self.zip_code = zip_code
+        self.inputString = input_string
 
 
-def main():
+ageRange = [300, 0]
+
+
+def get_user_data():
+    """
+    This function collects user data from a file, and puts into the format of the User class.
+    It also does some formatting, and calculates the AGO value for comparison.
+    :return:
+    A list of User class objects with some data populated.
+    """
     f = open("ml-100k/u.user", 'r')
     users = []
-    ageRange = [300, 0]
     for line in f.readlines():
         u = line.split("|")
         u[0] = int(u[0])
@@ -48,23 +58,27 @@ def main():
 
     for user in users:
         user.ageGenderOccupationValue = calculateAGOValue(user, ageRange)
+    return users
 
-    me = User(1000, 22, 1, "programmer", "04473", "")
+
+def main():
+    users = get_user_data()
+
+    me = User(1000, 22, 1, "programmer", "04473", "")               # Create a user class for me to compare.
     me.ageGenderOccupationValue = calculateAGOValue(me, ageRange)
     closestUser = [None, None, None]
     cUVal = [20, 20, 20]
 
-    for user in users:
+    for user in users:                                              # Compares every user to me, and picks the top three
         for i in range(len(closestUser)):
             if abs(user.ageGenderOccupationValue - me.ageGenderOccupationValue) < cUVal[i] and user not in closestUser:
                 cUVal[i] = abs(user.ageGenderOccupationValue - me.ageGenderOccupationValue)
                 user.reviews = getReviewsByUser(user.user_id)
                 closestUser[i] = user
 
-
-    for user in closestUser:
-        print(user.inputString.split("\n")[0])
-        user.reviews = sorted(user.reviews, key=lambda x: x[1])
+    for user in closestUser:                                        # For each of the close users, this section finds
+        print(user.inputString.split("\n")[0])                      # (cont) the highest and lowest rated movies the
+        user.reviews = sorted(user.reviews, key=lambda x: x[1])     # (cont) user has seen.
 
         topThree = []
         bottomThree = []
@@ -74,19 +88,26 @@ def main():
             if len(user.reviews) - i < 4:
                 topThree.append(user.reviews[i])
 
-        print("User's top three movies:")
-        for movie in topThree:
+        print("User's top three movies:")                           # This section prints the top three and bottom three
+        for movie in topThree:                                      # (cont) reviewed movies that the user has seen.
             print(getMovieTitle(movie[0]))
 
-        print
-
-        print("User's bottom three movies:")
+        print("\nUser's bottom three movies:")
         for movie in bottomThree:
             print(getMovieTitle(movie[0]))
         print("\n"*2)
 
 
 def getMovieTitle(movieID):
+    """
+    This function gets the movie title from the movie id
+
+    :param movieID:
+    The movie ID as found in u.item from the data set as an int
+
+    :return:
+    The movie title as a string
+    """
     f = open("ml-100k/u.item")
 
     for line in f.readlines():
@@ -97,6 +118,16 @@ def getMovieTitle(movieID):
 
 
 def getReviewsByUser(UID):
+    """
+    Gets all the reviews that a user has done.
+    :param UID:
+    The ID of the user as found in u.data, as an integer
+
+    :return:
+    A list containing two items:
+    (1) The ID number of the movie.
+    (2) The rating that the user left for the movie.
+    """
     f = open("ml-100k/u.data")
     output = []
     for line in f.readlines():
@@ -108,6 +139,19 @@ def getReviewsByUser(UID):
 
 
 def calculateAGOValue(user, ageRange):
+    """
+    Calculates an aggregate score from user data on age, occupation, and gender. Assumed to be a bad system, needs
+    to be fixed.
+
+    :param user:
+    A User class object for which the score will be calculated.
+
+    :param ageRange:
+    The range of ages found in the data set (to calculate with the smallest numbers possible)
+
+    :return:
+    An integer that represents the aggregate score.
+    """
     ageNorm = 0.5
     occupationNorm = 0.5
     if user.age != None:
@@ -125,7 +169,7 @@ def occupation_to_val(input):               # Arbitrarily sets value of occupati
     if input == "administrator":            # None is a neutral value. It's something that may be too variable to be
         return 21                           # (continued)  classified.
     elif input == "artist":
-        return 1
+        return 1                            # This is largely based on personal bias on how different jobs are similar.
     elif input == "doctor":
         return 26
     elif input == "educator":
